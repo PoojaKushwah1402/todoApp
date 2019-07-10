62d2abc483e2a92a3e6614ac083499fce1e2d8f1
Pooja <pooja@Poojas-MacBook-Air.local>
2019-07-11 01:08:11 +0400

    refactor code

---
+++ handler.js
@@ -0,0 +1,89 @@
+(function () {
+    function createSingleTodoStructure(todo) {
+        let newdiv = document.createElement('div');
+        newdiv.className = 'injs';
+        newdiv.textContent = todo.name;
+
+
+        let input = document.createElement('input');
+        input.type = "checkbox";
+        input.checked = todo.isCompleted;
+        input.onchange = function() {
+            window.todos.toggleTodoState(todo.id);
+        };
+        input.className='checkboxround';
+        newdiv.appendChild(input);
+        return newdiv;
+    }
+
+    const todoInputSetup = function() {
+        const inputElement = document.querySelector("#myInput");
+        if (inputElement) {
+            inputElement.addEventListener("keydown", function(event) {
+                if(event.keyCode==13) {
+                    const name = event.currentTarget.value;
+                    
+                    if(name !== "") {
+                        const todoObj = window.todos.add(name);
+                        const div = createSingleTodoStructure(todoObj);
+                        const parent = document.getElementById('listItem');
+                        parent.appendChild(div);
+                        event.currentTarget.value = ''; 
+                        document.getElementById('second').innerHTML= window.todos.getFilteredTodos().length + " Item Left";
+                    }
+                }
+            })
+        } else {
+            throw new Error("input element not found on line no 6 handler.js");
+        }
+    }
+
+
+    const allFilter = function () {
+        const allFilter = document.querySelector("#allFilter");
+        if (allFilter) {
+            allFilter.addEventListener("click", function() {
+                window.todos.applyFilter("ALL");
+                const allTodos = window.todos.getAllTodos();
+                const parent = document.getElementById('listItem');
+                parent.innerHTML = '';
+
+                for(i = 0;i < allTodos.length; i++) {
+                    const div = createSingleTodoStructure(allTodos[i]);
+                    parent.appendChild(div);
+                }
+                document.getElementById('second').innerHTML= allTodos.length + " Item Left";
+            });
+        } else {
+            throw new Error("all filter element not found on line no 45 handler.js");
+        }
+    }
+
+
+    const activeFilter = function () {
+        const activeFilter = document.querySelector("#activeFilter");
+        if (activeFilter) {
+            activeFilter.addEventListener("click", function() {
+                window.todos.applyFilter("ACTIVE");
+                const allTodos = window.todos.getAllActive();
+                const parent = document.getElementById('listItem');
+                parent.innerHTML = '';
+
+                for(i = 0;i < allTodos.length; i++) {
+                    const div = createSingleTodoStructure(allTodos[i]);
+                    parent.appendChild(div);
+                }
+
+                document.getElementById('second').innerHTML= allTodos.length + " Item Left";
+            });
+        } else {
+            throw new Error("all filter element not found on line no 45 handler.js");
+        }
+    }
+
+
+
+    allFilter();
+    activeFilter();
+    todoInputSetup();
+})()
---
+++ model.js
@@ -0,0 +1,58 @@
+window.todos = (function () {
+    var todo = [];
+    var state = "ALL";
+    return {
+        applyFilter: function (filterName) {
+            state = filterName;
+        },
+        getFilter: function() {
+            return state;
+        },
+        getFilteredTodos: function () {
+            if (state === "ALL") return todo;
+
+            if (state === "ACTIVE") return window.todos.getAllActive();
+        },
+        add: function(name) {
+            const singleTodo = {
+                name: name,
+                id: Math.random(),
+                isCompleted: false
+            };
+
+            todo.push(singleTodo);
+
+            return singleTodo;
+        },
+        getAllTodos: function() {
+            return todo;
+        },
+        getAllActive: function() {
+            var filteredTodo = todo.filter(function(todoObj) {
+                return !todoObj.isCompleted;
+            });
+
+            return filteredTodo;
+        },
+        getAllCompleted: function() {
+            var filteredTodo = todo.filter(function(todoObj) {
+                return todoObj.isCompleted;
+            });
+
+            return filteredTodo;
+        },
+        clearCompletedTodos: function () {
+            todo = todo.filter(function(todoObj) {
+                return !todoObj.isCompleted;
+            });
+        },
+        toggleTodoState: function (id) {
+            todo = todo.map(function(todoObj) {
+                if (todoObj.id === id) {
+                    todoObj.isCompleted = !todoObj.isCompleted;
+                }
+                return todoObj;
+            });
+        }
+    };
+})();
